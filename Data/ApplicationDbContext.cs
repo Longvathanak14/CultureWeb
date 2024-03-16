b858bfa76c68b2064025ebf56a891833ff200f4b
Longvathanak14 <111101964+Longvathanak14@users.noreply.github.com>
2024-03-16 12:48:30 +0700

    new project

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -29,7 +29,8 @@ namespace CultureWeb.Data
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<Purchase> Purchases { get; set; }
         public DbSet<PurchaseDetail> PurchaseDetails { get; set; }
-        public DbSet<ProductPrice> ProductPrices { get; set; }       
+        public DbSet<ProductPrice> ProductPrices { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
 
         //protected override void OnModelCreating(ModelBuilder modelBuilder)
         //{
